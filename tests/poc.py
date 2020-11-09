acb20056e12723833f6144217aeb979c7678dc87
ajbalogh <andy.balogh@keysight.com>
2020-11-09 11:52:28 -0800

    reorganize poc script

--- tests/poc.py
+++ tests/poc.py
@@ -2,9 +2,8 @@ import snappi
 import pandas
 
 api = snappi.Api()
-config_state = api.control_state.config_state
-config_state.state = snappi.CONFIGSTATE_SET
-config = config_state.config
+
+config = api.control_state.config_state.config
 config.ports.append(name='Tx Port', location='10.36.74.26;02;13')
 config.ports.append(name='Rx Port', location='10.36.74.26;02;14')
 flow = config.flows.append(name='Tx -> Rx Flow')
@@ -17,10 +16,12 @@ flow.packet.append(choice=snappi.FLOWHEADER_ETHERNET) \
 flow.size.fixed = 128
 flow.duration.fixed_packets = 10000
 flow.rate.pps = 1000
+
+api.control_state.config_state.state = snappi.CONTROLCONFIGSTATE_SET
 api.set_state(api.control_state)
 
-control_state.flow_transmit_state.state = snappi.FLOWTRANSMITSTATE_START
-api.set_state(control_state)
+api.control_state.flow_transmit_state.state = snappi.FLOWTRANSMITSTATE_START
+api.set_state(api.control_state)
 
 request = api.result_portrequest.column_names = [
     snappi.RESULTPORTREQUEST_NAME, snappi.RESULTPORTREQUEST_LOCATION,
