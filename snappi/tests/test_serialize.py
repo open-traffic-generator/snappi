fb7de9457b0442cf4aef7c72bdd74004cb99ce0a
ajbalogh <andy.balogh@keysight.com>
2020-12-08 07:18:27 -0800

    updates

--- snappi/tests/test_serialize.py
+++ snappi/tests/test_serialize.py
@@ -9,13 +9,13 @@ def test_serialize_deserialize(api):
     control_state.set(choice='config_state')
     config = control_state.config_state.set(state='set').config
     config.options.port_options.set(location_preemption=True)
-    config.ports.append(name='Tx Port', location='10.36.74.26;02;13')
-    config.ports.append(name='Rx Port', location='10.36.74.26;02;14')
-    flow = config.flows.append(name='Tx -> Rx Flow')
+    config.ports.port(name='Tx Port', location='10.36.74.26;02;13')
+    config.ports.port(name='Rx Port', location='10.36.74.26;02;14')
+    flow = config.flows.flow(name='Tx -> Rx Flow')
     flow.tx_rx.set(choice='port')
     flow.tx_rx.port.set(tx_port_name=config.ports[0].name,
                         rx_port_name=config.ports[1].name)
-    flow.packet.append(choice='ethernet').append(choice='ipv4')
+    flow.packet.flow_header(choice='ethernet').flow_header(choice='ipv4')
 
     # serialize as JSON and print to stdout
     serialization = control_state.encode(encoding='json')
