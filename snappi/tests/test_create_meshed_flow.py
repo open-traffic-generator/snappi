df363cffafd6ed2639119795a904b8d1321c4f7c
ajbalogh <andy.balogh@keysight.com>
2021-01-22 16:13:37 -0800

    add test

---
+++ snappi/tests/test_create_meshed_flow.py
@@ -0,0 +1,27 @@
+import pytest
+
+
+def test_create_meshed_flow(api):
+    """Demonstrates a fully meshed configuration
+    """
+    config = api.config()
+
+    for i in range(1, 33):
+        config.ports.port(name='Port %s' % i, location='localhost/%s' % i)
+        device = config.devices.device(name='Device %s' % i)[-1]
+        device.ipv4.name = 'Ipv4 %s' % i
+        device.ethernet.name = 'Eth %s' % i
+
+    flow = config.flows.flow(name='Fully Meshed Flow')[0]
+    flow.tx_rx.device.tx_names = [device.name for device in config.devices]
+    flow.tx_rx.device.rx_names = [device.name for device in config.devices]
+    flow.size.fixed = 128
+    flow.rate.pps = 1000
+    flow.duration.fixed_packets.packets = 10000
+    flow.packet.ethernet().vlan().ipv4().tcp()
+
+    print(config)
+
+
+if __name__ == '__main__':
+    pytest.main(['-vv', '-s', __file__])
