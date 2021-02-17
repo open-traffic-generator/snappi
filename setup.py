35a8d654522d0bc6a836eb696605a49b58d1eb76
Ashutosh Kumar <ashutshkumr@gmail.com>
2021-02-17 20:14:22 +0530

    version 0.1.43

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setuptools.setup(
         'typing'
     ],
     extras_require={
-        'ixnetwork': ['snappi_ixnetwork==0.1.13']
+        'ixnetwork': ['snappi_ixnetwork==0.1.14']
     },
     tests_require=['pytest']
 )
