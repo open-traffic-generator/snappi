a8c832f55c0bd5e4fe40de7caf1948a03ff0f58a
Ashutosh Kumar <ashutshkumr@gmail.com>
2021-06-25 21:32:24 +0530

    version 0.4.12

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import requests
 import shutil
 
 pkg_name = "snappi"
-version = "0.4.11"
+version = "0.4.12"
 models_version = "0.4.4"
 
 # read long description from readme.md
@@ -83,7 +83,7 @@ setuptools.setup(
         'typing'
     ],
     extras_require={
-        'ixnetwork': ['snappi_ixnetwork==0.4.0'],
+        'ixnetwork': ['snappi_ixnetwork==0.4.4'],
         'convergence': ['snappi_convergence==0.0.14'],
         'testing': ['pytest', 'flask']
     }
