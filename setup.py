b8635f885f2423a51370ff79e91d633a04dd4b85
Fred <fredpower44@gmail.com>
2021-08-09 12:13:40 -0700

    updated 'trex' extras version

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ setuptools.setup(
     ],
     extras_require={
         'ixnetwork': ['snappi_ixnetwork==0.4.11'],
-        'trex': ['snappi_trex==0.0.124'],
+        'trex': ['snappi_trex'],
         'convergence': ['snappi_convergence==0.0.27'],
         'testing': ['pytest', 'flask']
     }
