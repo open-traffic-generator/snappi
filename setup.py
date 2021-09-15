3896dd70e90a8396d0c3370e77b7c8acf11e95c3
ANISH-GOTTAPU <48308607+ANISH-GOTTAPU@users.noreply.github.com>
2021-09-15 11:19:58 +0530

    Version 0.5.8
    Version 0.5.8

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ import shutil
 
 pkg_name = "snappi"
 go_pkg_name = "gosnappi"
-version = "0.5.7"
-models_version = "0.5.6"
+version = "0.5.8"
+models_version = "0.5.7"
 
 # read long description from readme.md
 base_dir = os.path.abspath(os.path.dirname(__file__))
