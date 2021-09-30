c721cc0fa490cfdf2508ae92e5cdf9ac346b1988
Rangababu-R <72373312+Rangababu-R@users.noreply.github.com>
2021-09-30 13:04:09 +0530

    models v0.6.3 and snappi v0.6.2

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ import shutil
 
 pkg_name = "snappi"
 go_pkg_name = "gosnappi"
-version = "0.6.1"
-models_version = "0.6.1"
+version = "0.6.2"
+models_version = "0.6.3"
 
 # read long description from readme.md
 base_dir = os.path.abspath(os.path.dirname(__file__))
