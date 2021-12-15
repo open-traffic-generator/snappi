96b9fc8ce4eab6ad4fa4257f42fc06c3500aa350
ANISH-GOTTAPU <48308607+ANISH-GOTTAPU@users.noreply.github.com>
2021-12-15 19:18:25 +0530

    Version 0.7.3
    Version 0.7.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import shutil
 pkg_name = "snappi"
 go_pkg_name = "gosnappi"
 model_protobuf_name = "otg"
-version = "0.7.2"
+version = "0.7.3"
 models_version = "0.7.2"
 
 # read long description from readme.md
@@ -94,7 +94,7 @@ setuptools.setup(
         "typing-extensions",
     ],
     extras_require={
-        "ixnetwork": ["snappi_ixnetwork==0.7.1"],
+        "ixnetwork": ["snappi_ixnetwork==0.7.2"],
         "trex": ["snappi_trex"],
         "convergence": ["snappi_convergence==0.2.2"],
         "testing": ["pytest", "flask"],
