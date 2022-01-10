a733abd0663123633d2c5d4f0d161e0d2ba78cde
alakendu <alakendu.jana@keysight.com>
2022-01-10 20:40:32 +0530

    version 0.7.5

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import shutil
 pkg_name = "snappi"
 go_pkg_name = "gosnappi"
 model_protobuf_name = "otg"
-version = "0.7.4"
+version = "0.7.5"
 models_version = "0.7.2"
 
 # read long description from readme.md
