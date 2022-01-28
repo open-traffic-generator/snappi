1bf9d9c4b1e6202eb00554d79aa50bdec173ae52
Ashutosh Kumar <ashutshkumr@gmail.com>
2022-01-28 13:15:55 +0530

    version 0.7.7

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ import shutil
 pkg_name = "snappi"
 go_pkg_name = "gosnappi"
 model_protobuf_name = "otg"
-version = "0.7.6"
-models_version = "0.7.2"
+version = "0.7.7"
+models_version = "0.7.3"
 
 # read long description from readme.md
 base_dir = os.path.abspath(os.path.dirname(__file__))
