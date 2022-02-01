68acb1c96eb7198064701ef0617658313c65771b
ANISH-GOTTAPU <48308607+ANISH-GOTTAPU@users.noreply.github.com>
2022-02-01 16:01:41 +0530

    Version 0.7.9
    Version 0.7.9

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import shutil
 pkg_name = "snappi"
 go_pkg_name = "gosnappi"
 model_protobuf_name = "otg"
-version = "0.7.8"
+version = "0.7.9"
 models_version = "0.7.3"
 
 # read long description from readme.md
