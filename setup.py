828412738609bd6d1cd223dc8b18eb0587ab476f
ANISH-GOTTAPU <48308607+ANISH-GOTTAPU@users.noreply.github.com>
2022-03-14 16:25:44 +0530

    Version 0.7.23
    Version 0.7.23

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import shutil
 pkg_name = "snappi"
 go_pkg_name = "gosnappi"
 model_protobuf_name = "otg"
-version = "0.7.22"
+version = "0.7.23"
 models_version = "0.7.8"
 
 # read long description from readme.md
@@ -96,7 +96,7 @@ setuptools.setup(
         "protobuf==3.15.0" # Warning - changing versions need thorough investigation
     ],
     extras_require={
-        "ixnetwork": ["snappi_ixnetwork==0.7.5"],
+        "ixnetwork": ["snappi_ixnetwork==0.7.6"],
         "trex": ["snappi_trex"],
         "convergence": ["snappi_convergence==0.2.2"],
         "testing": ["pytest", "flask"],
