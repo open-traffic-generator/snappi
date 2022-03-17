77c1a741f9857d7eed67fbde48e77f77a139f8df
Rangababu-R <72373312+Rangababu-R@users.noreply.github.com>
2022-03-17 10:17:09 +0530

    v0.7.25

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import shutil
 pkg_name = "snappi"
 go_pkg_name = "gosnappi"
 model_protobuf_name = "otg"
-version = "0.7.24"
+version = "0.7.25"
 models_version = "0.7.9"
 
 # read long description from readme.md
@@ -96,7 +96,7 @@ setuptools.setup(
         "protobuf==3.15.0" # Warning - changing versions need thorough investigation
     ],
     extras_require={
-        "ixnetwork": ["snappi_ixnetwork==0.7.6"],
+        "ixnetwork": ["snappi_ixnetwork==0.7.7"],
         "trex": ["snappi_trex"],
         "convergence": ["snappi_convergence==0.2.2"],
         "testing": ["pytest", "flask"],
