711ce9871441792d180d66638dbc72db252b48e5
ANISH-GOTTAPU <48308607+ANISH-GOTTAPU@users.noreply.github.com>
2022-05-09 19:26:39 +0530

    Version 0.7.34

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ import shutil
 pkg_name = "snappi"
 go_pkg_name = "gosnappi"
 model_protobuf_name = "otg"
-version = "0.7.33"
-models_version = "0.7.12"
+version = "0.7.34"
+models_version = "0.7.13"
 
 # read long description from readme.md
 base_dir = os.path.abspath(os.path.dirname(__file__))
