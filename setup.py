3f12aa34aa6bdcc434df257b7ab7e975d65c3abe
Ashutosh Kumar <ashutshkumr@gmail.com>
2022-05-19 22:45:07 +0530

    version 0.7.35

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import shutil
 pkg_name = "snappi"
 go_pkg_name = "gosnappi"
 model_protobuf_name = "otg"
-version = "0.7.34"
+version = "0.7.35"
 models_version = "0.7.13"
 
 # read long description from readme.md
