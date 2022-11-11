cbf995bb94f817b5a718238acdcde63f3a3f61f9
Ashutosh Kumar <ashutshkumr@gmail.com>
2022-11-11 13:56:56 +0530

    version 0.9.6

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ import shutil
 pkg_name = "snappi"
 go_pkg_name = "gosnappi"
 model_protobuf_name = "otg"
-version = "0.9.5"
-models_version = "0.9.2"
+version = "0.9.6"
+models_version = "0.9.7"
 
 # read long description from readme.md
 base_dir = os.path.abspath(os.path.dirname(__file__))
