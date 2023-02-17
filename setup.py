679e007ef24f65e998b5b54ead3d83211d786349
Ashutosh Kumar <ashutshkumr@gmail.com>
2023-02-17 14:43:30 +0000

    upgraded models and openapiart

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ import shutil
 pkg_name = "snappi"
 go_pkg_name = "gosnappi"
 model_protobuf_name = "otg"
-version = "0.10.7"
-models_version = "0.10.9"
+version = "0.10.8"
+models_version = "0.10.10"
 
 # supported values - local openapiart path or None
 USE_OPENAPIART_DIR = None
