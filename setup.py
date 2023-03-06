6c0edad1287ce81d07d3ca6cbfe3e9c34c79dbdd
vhowdhur <vibaswan.roychowdhury@keysight.com>
2023-03-06 16:01:25 +0530

    switch open api art branch to x-status-warnings

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ USE_OPENAPIART_DIR = None
 USE_MODELS_DIR = None
 
 # supported values - branch name or None
-USE_OPENAPIART_BRANCH = None
+USE_OPENAPIART_BRANCH = "x-status-warnings"
 USE_MODELS_BRANCH = "control-api-refactor"
 
 OPENAPIART_REPO = "https://github.com/open-traffic-generator/openapiart.git"
