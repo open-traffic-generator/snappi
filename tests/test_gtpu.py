b025e9ce8dcfa2c3484fd62474506d4bbd5102ca
dipendughosh <dipendughosh@gmail.com>
2023-03-16 16:15:59 +0530

    fix build break

--- tests/test_gtpu.py
+++ tests/test_gtpu.py
@@ -50,7 +50,8 @@ import json
 import collections
 
 # these test parameters would normally come from a test infrastructure file
-d = yaml.safe_load("""
+d = yaml.safe_load(
+    """
 # gnb_mac_addr This is the MAC SA for uplink packets & MAC DA for downlink packets
 # n3_mac_addr This is the MAC DA for uplink packets & MAC SA for downlink packets
 globals:
@@ -76,11 +77,12 @@ servers:
   - location: localuhd/2
     teid: 1025
     ue_ip: 1.1.101.1
-""")
+"""
+)
 d = json.dumps(d)
 test_params = json.loads(
-    d,
-    object_hook=lambda d: collections.namedtuple('X', d.keys())(*d.values()))
+    d, object_hook=lambda d: collections.namedtuple("X", d.keys())(*d.values())
+)
 
 
 def test_gtpu(api):
@@ -88,38 +90,66 @@ def test_gtpu(api):
     config = api.config()
 
     for user, server in zip(test_params.users, test_params.servers):
-        config.ports \
-            .port(name='User Port %s' % user.location, location=user.location) \
-            .port(name='Server Port %s' % server.location, location=server.location)
+        config.ports.port(
+            name="User Port %s" % user.location, location=user.location
+        ).port(name="Server Port %s" % server.location, location=server.location)
         tx_port = config.ports[-2]
         rx_port = config.ports[-1]
-        create_flow(config, 'Uplink', user, server, tx_port, rx_port,
-                    test_params.globals.gnb_mac_addr.replace(' ', ':'),
-                    test_params.globals.n3_mac_addr.replace(' ', ':'),
-                    test_params.globals.gnb_ip_addr,
-                    test_params.globals.upf_ip_addr, user.ue_ip,
-                    test_params.globals.inner_ip_src_dst,
-                    test_params.globals.gtp_tunnels)
-        create_flow(config, 'Downlink', server, user, tx_port, rx_port,
-                    test_params.globals.n3_mac_addr.replace(' ', ':'),
-                    test_params.globals.gnb_mac_addr.replace(' ', ':'),
-                    test_params.globals.upf_ip_addr,
-                    test_params.globals.gnb_ip_addr,
-                    test_params.globals.inner_ip_src_dst, server.ue_ip,
-                    test_params.globals.gtp_tunnels)
+        create_flow(
+            config,
+            "Uplink",
+            user,
+            server,
+            tx_port,
+            rx_port,
+            test_params.globals.gnb_mac_addr.replace(" ", ":"),
+            test_params.globals.n3_mac_addr.replace(" ", ":"),
+            test_params.globals.gnb_ip_addr,
+            test_params.globals.upf_ip_addr,
+            user.ue_ip,
+            test_params.globals.inner_ip_src_dst,
+            test_params.globals.gtp_tunnels,
+        )
+        create_flow(
+            config,
+            "Downlink",
+            server,
+            user,
+            tx_port,
+            rx_port,
+            test_params.globals.n3_mac_addr.replace(" ", ":"),
+            test_params.globals.gnb_mac_addr.replace(" ", ":"),
+            test_params.globals.upf_ip_addr,
+            test_params.globals.gnb_ip_addr,
+            test_params.globals.inner_ip_src_dst,
+            server.ue_ip,
+            test_params.globals.gtp_tunnels,
+        )
 
     api.set_config(config)
 
 
-def create_flow(config, flow_prefix, src_param, dst_param, tx_port, rx_port,
-                src_mac, dst_mac, outer_ip_src, outer_ip_dst, inner_ip_src,
-                inner_ip_dst, gtp_tunnels):
-    flow_name = '%s %s -> %s' % (flow_prefix, tx_port.name, rx_port.name)
+def create_flow(
+    config,
+    flow_prefix,
+    src_param,
+    dst_param,
+    tx_port,
+    rx_port,
+    src_mac,
+    dst_mac,
+    outer_ip_src,
+    outer_ip_dst,
+    inner_ip_src,
+    inner_ip_dst,
+    gtp_tunnels,
+):
+    flow_name = "%s %s -> %s" % (flow_prefix, tx_port.name, rx_port.name)
     flow = config.flows.flow(name=flow_name)[-1]
     flow.tx_rx.port.tx_name = tx_port.name
     flow.tx_rx.port.rx_name = rx_port.name
 
-    flow.packet.ingress.ethernet().ipv4().udp().gtpv1().ipv4()
+    flow.packet.ethernet().ipv4().udp().gtpv1().ipv4()
     eth, outer_ip, udp, gtp, inner_ip = flow.packet.ingress
 
     eth.src.value = src_mac
@@ -131,12 +161,12 @@ def create_flow(config, flow_prefix, src_param, dst_param, tx_port, rx_port,
     gtp.teid.increment.count = gtp_tunnels
     inner_ip.src.value = inner_ip_src
     inner_ip.dst.value = inner_ip_dst
-    gtp.teid.metric_tags.add(name='gtp_teid')
+    gtp.teid.metric_tags.add(name="gtp_teid")
 
     flow.size.fixed = 128
     flow.rate.gbps = 1
     flow.duration.fixed_packets.packets = 100000
 
 
-if __name__ == '__main__':
-    pytest.main(['-vv', '-s', __file__])
+if __name__ == "__main__":
+    pytest.main(["-vv", "-s", __file__])
