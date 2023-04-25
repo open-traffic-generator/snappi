2244cd6937a437fda900c423f74d2015839da4c9
vhowdhur <vibaswan.roychowdhury@keysight.com>
2023-04-25 14:38:20 +0530

    bug fix

--- do.py
+++ do.py
@@ -36,8 +36,8 @@ USE_OPENAPIART_DIR = None
 USE_MODELS_DIR = None
 
 # supported values - branch name or None
-USE_OPENAPIART_BRANCH = None
-USE_MODELS_BRANCH = None
+USE_OPENAPIART_BRANCH = "isolate-grpc-from-rest-python"
+USE_MODELS_BRANCH = "isolate-grpc-from-rest-python"
 
 OPENAPIART_REPO = "https://github.com/open-traffic-generator/openapiart.git"
 MODELS_REPO = "https://github.com/open-traffic-generator/models.git"
