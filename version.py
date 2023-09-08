0ec9a031cddfc04927794e86eafa39c25387be53
Apratim Mukherjee <apratim.mukherjee@keysight.com>
2023-09-08 12:53:39 +0530

    Upgrading to OTG 0.12.3 - Revamped Extended Community

--- version.py
+++ version.py
@@ -2,8 +2,8 @@
 
 
 class Version:
-    models_version = "0.12.2"
-    version = "0.12.2"
+    models_version = "0.12.3"
+    version = "0.12.3"
     package_name = "snappi"
     protobuf_name = "otg"
     go_package_name = "gosnappi"
