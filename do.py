6c72d4a1eaa2e4349134c1a59081db6b02979ad4
Rudranil <rudranil.das@keysight.com>
2023-10-16 16:00:45 +0000

    fix typo

--- do.py
+++ do.py
@@ -36,8 +36,8 @@ USE_OPENAPIART_DIR = None
 USE_MODELS_DIR = None
 
 # supported values - branch name or None
-USE_OPENAPIART_BRANCH = main
-USE_MODELS_BRANCH = dev-fly-controlstate
+USE_OPENAPIART_BRANCH = "main"
+USE_MODELS_BRANCH = "dev-fly-controlstate"
 
 OPENAPIART_REPO = "https://github.com/open-traffic-generator/openapiart.git"
 MODELS_REPO = "https://github.com/open-traffic-generator/models.git"
