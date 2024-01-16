9da0fa924e62fc3378bde75aefa2e39db432d459
Ashutosh Kumar <ashutshkumr@gmail.com>
2024-01-16 16:35:53 +0530

    v0.13.6

--- version.py
+++ version.py
@@ -2,8 +2,8 @@
 
 
 class Version:
-    models_version = "0.13.5"
-    version = "0.13.5"
+    models_version = "0.13.6"
+    version = "0.13.6"
     package_name = "snappi"
     protobuf_name = "otg"
     go_package_name = "gosnappi"
