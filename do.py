6df69ab6c24c5f12e496f0aecdd46a4b868ffd88
vhowdhur <vibaswan.roychowdhury@keysight.com>
2024-05-08 10:00:13 +0530

    update to latest openapiart version

--- do.py
+++ do.py
@@ -36,7 +36,7 @@ USE_OPENAPIART_DIR = None
 USE_MODELS_DIR = None
 
 # supported values - branch name or None
-USE_OPENAPIART_BRANCH = "enhance_x_field_auto"
+USE_OPENAPIART_BRANCH = None
 USE_MODELS_BRANCH = "traffic_over_dhcp_interface"
 
 OPENAPIART_REPO = "https://github.com/open-traffic-generator/openapiart.git"
