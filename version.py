ce3be618d3a1780f06deb8ce9f8d2c23bb476bda
Apratim Mukherjee <78898747+apratimmukherjee@users.noreply.github.com>
2024-07-24 18:11:34 +0530

    v1.7.0

--- version.py
+++ version.py
@@ -2,8 +2,8 @@
 
 
 class Version:
-    models_version = "1.6.2"
-    version = "1.6.2"
+    models_version = "1.7.0"
+    version = "1.7.0"
     package_name = "snappi"
     protobuf_name = "otg"
     go_package_name = "gosnappi"
