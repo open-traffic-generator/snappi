ba812dfc5ad23bd87dbc31a150de7deb34d2d1a8
Apratim Mukherjee <78898747+apratimmukherjee@users.noreply.github.com>
2024-07-26 12:02:32 +0530

    v1.7.2
    gosnappi split support.

--- version.py
+++ version.py
@@ -2,8 +2,8 @@
 
 
 class Version:
-    models_version = "1.7.0"
-    version = "1.7.1"
+    models_version = "1.7.1"
+    version = "1.7.2"
     package_name = "snappi"
     protobuf_name = "otg"
     go_package_name = "gosnappi"
