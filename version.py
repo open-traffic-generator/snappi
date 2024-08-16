f4abc852f76eb52d3f4fce0a9a82ea37c45a89b2
Apratim Mukherjee <78898747+apratimmukherjee@users.noreply.github.com>
2024-08-16 10:52:13 +0530

    v1.8.0
    DHCPv6 support

--- version.py
+++ version.py
@@ -2,8 +2,8 @@
 
 
 class Version:
-    models_version = "1.7.1"
-    version = "1.7.2"
+    models_version = "1.8.0"
+    version = "1.8.0"
     package_name = "snappi"
     protobuf_name = "otg"
     go_package_name = "gosnappi"
