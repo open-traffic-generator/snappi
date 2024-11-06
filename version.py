55c2785aa27356536e9eaca3ceb2ab4cc112f662
Apratim Mukherjee <78898747+apratimmukherjee@users.noreply.github.com>
2024-11-06 13:31:52 +0530

    v1.16.0
    Simulated Topology support.

--- version.py
+++ version.py
@@ -2,8 +2,8 @@
 
 
 class Version:
-    models_version = "1.15.0"
-    version = "1.15.0"
+    models_version = "1.16.0"
+    version = "1.16.0"
     package_name = "snappi"
     protobuf_name = "otg"
     go_package_name = "gosnappi"
