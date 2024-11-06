5bb6d776d67eb99a68f908ff79ad747438a0f3b3
Apratim Mukherjee <78898747+apratimmukherjee@users.noreply.github.com>
2024-11-06 11:05:05 +0530

    v1.15.0
    LLDP - Support for fetching value field of Org TLVs in get_state.

--- version.py
+++ version.py
@@ -2,8 +2,8 @@
 
 
 class Version:
-    models_version = "1.14.0"
-    version = "1.14.0"
+    models_version = "1.15.0"
+    version = "1.15.0"
     package_name = "snappi"
     protobuf_name = "otg"
     go_package_name = "gosnappi"
