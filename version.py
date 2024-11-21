79b8d4775fcce94bd431c4347a3834274070d8f2
Apratim Mukherjee <78898747+apratimmukherjee@users.noreply.github.com>
2024-11-21 16:26:22 +0530

    v1.17.0
    Support of 'auto' for protocol version field in GRE packet template for flows.

--- version.py
+++ version.py
@@ -2,8 +2,8 @@
 
 
 class Version:
-    models_version = "1.16.0"
-    version = "1.16.0"
+    models_version = "1.17.0"
+    version = "1.17.0"
     package_name = "snappi"
     protobuf_name = "otg"
     go_package_name = "gosnappi"
