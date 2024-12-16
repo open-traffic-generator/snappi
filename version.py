547d36f52b54e681d9926112a4d6ff578737a262
Apratim Mukherjee <78898747+apratimmukherjee@users.noreply.github.com>
2024-12-16 18:03:02 +0530

    v1.19.0
    Support for port last change timestamp

--- version.py
+++ version.py
@@ -2,8 +2,8 @@
 
 
 class Version:
-    models_version = "1.18.0"
-    version = "1.18.0"
+    models_version = "1.19.0"
+    version = "1.19.0"
     package_name = "snappi"
     protobuf_name = "otg"
     go_package_name = "gosnappi"
