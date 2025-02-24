62a261bcf7ebdf92a9312ce55eba90a7ac80b7b7
itachary <indrani.bhattacharya@keysight.com>
2025-02-24 13:45:02 +0000

    Update snappi-ixn version

--- version.py
+++ version.py
@@ -3,7 +3,7 @@
 
 class Version:
     models_version = "1.21.0"
-    version = "1.21.0"
+    version = "1.21.1"
     package_name = "snappi"
     protobuf_name = "otg"
     go_package_name = "gosnappi"
