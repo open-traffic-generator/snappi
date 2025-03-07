124dd4f8d630fa7366803c14ddb15e997419a792
Apratim Mukherjee <78898747+apratimmukherjee@users.noreply.github.com>
2025-03-07 21:10:14 +0530

    v1.25.0
    Support of MacSec/ MKA

--- version.py
+++ version.py
@@ -2,8 +2,8 @@
 
 
 class Version:
-    models_version = "1.24.0"
-    version = "1.24.1"
+    models_version = "1.25.0"
+    version = "1.25.0"
     package_name = "snappi"
     protobuf_name = "otg"
     go_package_name = "gosnappi"
