816dacd79640bbf10e9b7039f7d704fefa65ade0
itachary <indrani.bhattacharya@keysight.com>
2025-03-12 19:12:43 +0000

    Update snappi-ixn

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setuptools.setup(
     python_requires=">=2.7, <4",
     install_requires=install_requires,
     extras_require={
-        "ixnetwork": ["snappi_ixnetwork==1.25.1"],
+        "ixnetwork": ["snappi_ixnetwork==1.26.0"],
         "trex": ["snappi_trex"],
         "testing": ["pytest", "flask"],
     },
