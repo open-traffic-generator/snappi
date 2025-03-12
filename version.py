5de4833a57a58219f2a6774507a2088e87faf4b4
Apratim Mukherjee <78898747+apratimmukherjee@users.noreply.github.com>
2025-03-12 18:09:50 +0530

    v1.26.0
    Bugfix for MACSec metrics response column names.

--- version.py
+++ version.py
@@ -2,8 +2,8 @@
 
 
 class Version:
-    models_version = "1.25.0"
-    version = "1.25.2"
+    models_version = "1.26.0"
+    version = "1.26.0"
     package_name = "snappi"
     protobuf_name = "otg"
     go_package_name = "gosnappi"
