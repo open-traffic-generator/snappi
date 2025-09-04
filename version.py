0639f912183f22a5e63cca35bf70d78aa02bc47b
Apratim Mukherjee <78898747+apratimmukherjee@users.noreply.github.com>
2025-09-04 12:57:58 +0530

    v1.35.0
    Support for payload configurations in Flows.

--- version.py
+++ version.py
@@ -2,8 +2,8 @@
 
 
 class Version:
-    models_version = "1.34.0"
-    version = "1.34.1"
+    models_version = "1.35.0"
+    version = "1.35.0"
     package_name = "snappi"
     protobuf_name = "otg"
     go_package_name = "gosnappi"
