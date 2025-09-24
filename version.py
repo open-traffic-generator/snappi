3771f3851e73b51b43bbbf8e7d0b573ec98be0da
Apratim Mukherjee <78898747+apratimmukherjee@users.noreply.github.com>
2025-09-24 18:08:49 +0530

    v1.40.0
    Support of IPv6 Segment Routing Extension Header in flows.

--- version.py
+++ version.py
@@ -2,8 +2,8 @@
 
 
 class Version:
-    models_version = "1.39.0"
-    version = "1.39.0"
+    models_version = "1.40.0"
+    version = "1.40.0"
     package_name = "snappi"
     protobuf_name = "otg"
     go_package_name = "gosnappi"
