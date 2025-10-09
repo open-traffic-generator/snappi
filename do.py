9342cc97c4eb9f9f0b932f96247452b8eb43c192
Apratim Mukherjee <78898747+apratimmukherjee@users.noreply.github.com>
2025-10-09 15:58:54 +0530

    Update do.py

--- do.py
+++ do.py
@@ -33,11 +33,11 @@ sdk_version = Version.version
 
 # supported values - local openapiart path or None
 USE_OPENAPIART_DIR = None
-USE_MODELS_DIR = "bmp"
+USE_MODELS_DIR = None
 
 # supported values - branch name or None
 USE_OPENAPIART_BRANCH = None
-USE_MODELS_BRANCH = None
+USE_MODELS_BRANCH = "bmp"
 
 OPENAPIART_REPO = "https://github.com/open-traffic-generator/openapiart.git"
 MODELS_REPO = "https://github.com/open-traffic-generator/models.git"
