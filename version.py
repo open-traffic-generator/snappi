4f9457d5c21625f0bec4a82554e30877302ed7d7
Apratim Mukherjee <78898747+apratimmukherjee@users.noreply.github.com>
2026-06-26 12:14:45 +0530

    v1.58.0
    Support for on-the-fly update ( enable/disable ) of Overload bit for selected Emulated or Simulated ISIS Routers.

--- version.py
+++ version.py
@@ -2,8 +2,8 @@
 
 
 class Version:
-    models_version = "1.57.0"
-    version = "1.57.1"
+    models_version = "1.58.0"
+    version = "1.58.0"
     package_name = "snappi"
     protobuf_name = "otg"
     go_package_name = "gosnappi"
