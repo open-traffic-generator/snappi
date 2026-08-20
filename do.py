e662f66d91137158123dd9aa3a649343277fa62e
Sourav Sinha-Roy <sourav.sinha-roy@keysight.com>
2026-08-20 13:41:32 +0530

    dummy

--- do.py
+++ do.py
@@ -37,7 +37,7 @@ USE_MODELS_DIR = None
 
 # supported values - branch name or None
 USE_OPENAPIART_BRANCH = None
-USE_MODELS_BRANCH = "ospf_sr"
+USE_MODELS_BRANCH = "ospf_s"
 
 OPENAPIART_REPO = "https://github.com/open-traffic-generator/openapiart.git"
 MODELS_REPO = "https://github.com/open-traffic-generator/models.git"
